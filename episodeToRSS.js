da43b80092e1970ef7aaad7fe89f63cbed52db97
Gil <gilbertjvirgo@gmail.com>
2023-01-17 18:14:58 +0000

    added escape html library

--- episodeToRSS.js
+++ episodeToRSS.js
@@ -1,9 +1,10 @@
 import * as prismicH from "@prismicio/helpers";
 
+import escape from "escape-html";
 import rfc822Date from "rfc822-date";
 
 export default ({ title, description, audio_url, original_date_published }) => {
-	return `<item>
+	return escape(`<item>
     <title>${title}</title>
     <description>${prismicH.asText(description)}</description>
     <itunes:image href="https://is5-ssl.mzstatic.com/image/thumb/Podcasts125/v4/f7/87/1f/f7871fc7-e872-8acc-ad53-a824e98ba7e2/mza_15772983630683249442.jpg/626x0w.webp"/>
@@ -12,5 +13,5 @@ export default ({ title, description, audio_url, original_date_published }) => {
     <itunes:duration>00:00:00</itunes:duration>
     <guid>${audio_url.url}</guid>
     <pubDate>${rfc822Date(new Date(original_date_published))}</pubDate>
-</item>`.replace(/&/g, "&amp;");
+</item>`);
 };
