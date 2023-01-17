667b7e9d2b749a95e25ac9a1644adf979ab673f2
Gil <gilbertjvirgo@gmail.com>
2023-01-17 17:35:19 +0000

    output.rss -> rss.xml

--- refreshRSSFeed.js
+++ refreshRSSFeed.js
@@ -13,7 +13,7 @@ const init = async () => {
 	const episodes = await client.getAllByType("podcast");
 
 	fs.writeFileSync(
-		"./output.rss",
+		"./rss.xml",
 		`<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
 <channel>
 <title>Christian Heritage London Podcast</title>
